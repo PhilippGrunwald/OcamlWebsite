4ae8b15ec1118b08b73bd9b321a9c5cf30875e85
PhilippGrunwald <phili@DESKTOP-JFOOLQF.localdomain>
2025-07-01 01:41:03 +0200

    first routing of the server

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let () =
+  Dream.run 
+  @@ Dream.logger
+  @@ Dream.router [
+
+    Dream.get "/" (fun _ -> Dream.html "Good Morning, world!");
+
+    Dream.get "/echo/:word"
+      (fun request -> Dream.html (Dream.param request "word"));
+  
+  ]
