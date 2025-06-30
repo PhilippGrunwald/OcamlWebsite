d7d6702c1d0ae4c1a838c17297765cf7b1bf0f7d
PhilippGrunwald <philipp050102@gmail.com>
2025-07-01 01:53:45 +0200

    first middleware

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,19 @@
+let count = ref 0
+
+let count_requests inner_handler request = 
+  count := !count + 1;
+  inner_handler request;;
+
+
 let () =
   Dream.run 
   @@ Dream.logger
+  @@ count_requests
   @@ Dream.router [
 
-    Dream.get "/" (fun _ -> Dream.html "Good Morning, world!");
+    Dream.get "/" (fun _ -> Dream.html @@ Printf.sprintf "Request: %i" !count);
 
     Dream.get "/echo/:word"
       (fun request -> Dream.html (Dream.param request "word"));
-  
+
   ]
